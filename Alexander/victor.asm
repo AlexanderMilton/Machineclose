ce5ff59847a76806e8e2f4c241ab6ab1e8b09402
Kiwi231 <alexandermiltons@gmail.com>
2014-04-11 17:17:46 +0200

    Created Pseudo code of InVertion Added Mars.jar to main folder for reasons of laziness

--- Alexander/victor.asm
+++ Alexander/victor.asm
@@ -20,78 +20,85 @@ data:
 	.word	0x00ee655b
 lf:
 	.word	10
+	
 .text
 	# t0 = index
 	# t1 = address
 	# t2 = offset
 	# t3 = current value
 	# t4 = array lenght
-	init:
-		li	$t0,	0
-		lw 	$t4,	datalen
-		li	$t2,	0
-		la	$t1,	data
+	
+init:
+		li	$t0,	0		# Iteration counter
+		la	$t1,	data		# Address pointer to data list
+		li	$t2,	0		# Offset (4 bytes) used to fetch values from list in address
+		# 	$t3 			holds the current value, used for printing. Initiated elsewhere
+		lw 	$t4,	datalen		# Length of array
 		
-		jal sort
-		nop
-	main:
-		add $t3,	$t2,	$t1		# Add offset and address to get the next value
+		jal 	sort			# Jump to sorting label
+		nop				# Avoid additional operation
 		
-		jal printElement
-		nop
+main:
+		add 	$t3,	$t2,	$t1	# Add offset and address to get the next value
 		
-		add	$t2,	$t2, 	4		# Increase the offset by 4 bytes (aka 32 bit)
+		jal 	printElement		# Print current values (stored in $t3)
+		nop				# Avoid additional operation
 		
-		add	$t0,	$t0,	1		# Increase the current iteration count
+		add	$t2,	$t2, 	4	# Increase the offset by 4 bytes (aka 32 bit)
+		
+		add	$t0,	$t0,	1	# Increase the current iteration count
 		blt	$t0,	$t4,	main	# Make sure we aren't outside of the array 
-		nop
+		nop				# Avoid additional operation
 		
-		li	$v0,	10				# Load the exit code
-		syscall
+		li	$v0,	10		# Load the exit code
+		syscall				# Exit program
 		
 printElement:
-		lw	$a0,	0($t3)			# Read the word from the address with an offset of 0 (we already added offset)
-		li	$v0,	1				# Add the code for print integer
+		lw	$a0,	0($t3)		# Read the word from the address with an offset of 0 (we already added offset)
+		li	$v0,	1		# Add the code for print integer
 		syscall
 		
-		lw	$a0,	lf				# Add a newline (linefeed) character
-		li	$v0,	11				# Add the code for print character
-		syscall
-		jr	$ra
-		nop
+		lw	$a0,	lf		# Add a newline (linefeed) character
+		li	$v0,	11		# Add the code for print character
+		syscall				
+		jr	$ra			# Return from jump
+		nop				# Avoid additional operation
 		
 sort:
 	# Init the counters
-	li	$s0,	0
-	li	$s1,	0
+	li	$s0,	0			# Outer pointer
+	li	$s1,	0			# Inner pointer
 	
-	sloop:
-	li	$s2,	0
-	li	$s3,	0
+sloop:
+	li	$s2,	0			# Reset pointer
+	li	$s3,	0			# Reset pointer
 	
-	mul	$s2,	$s0,	4
-	mul	$s3,	$s1,	4
+	mul	$s2,	$s0,	4		# Multiply outer pointer by 4, storing the address of a value in pointer $s2
+	mul	$s3,	$s1,	4		# Multiply inner pointer by 4, storing the address of a value in pointer $s3
 	
-	add $s2,	$t1,	$s2
-	add $s3,	$t1,	$s3
+	add 	$s2,	$t1,	$s2		# Add the adress list to the pointer $s2, store in $s2 
+	add 	$s3,	$t1,	$s3		# Add the adress list to the pointer $s3, store in $s3
 	
-	lw	$s4,	0($s2)
-	lw	$s5,	0($s3)
-	blt $s4,	$s5,	noswap
-	nop
+	lw	$s4,	0($s2)			# Load contents of $s2 to $s4 (no offset)
+	lw	$s5,	0($s3)			# Load contents of $s3 to $s5 (no offset)
+	blt 	$s4,	$s5,	noswap		# Check if $s4 is less than $s5. If true, branch to label noswap
+	nop					# Avoid additional operation
 	
-	sw	$s5,	0($s2)
-	sw	$s4,	0($s3)
+	# Swap values
+	sw	$s5,	0($s2)			# Store content of $s2 in $s5
+	sw	$s4,	0($s3)			# Store content of $s3 in $s4
 	
-	noswap:
-	add $s1,	$s1,	1
+noswap:
+	add 	$s1,	$s1,	1		# Increment address pointer by 1
 	
-	blt $s1,	$t4		sloop
-	nop
+	blt 	$s1,	$t4	sloop		# Check if the inner pointer < lenght of the data list. If true, perform a new loop
+	nop					# Avoid additional operation
 	
-	add $s0,	$s0,	1
-	blt	$s0,	$t4,	sloop
-	add $s1,	$s0,	$0
+	add 	$s0,	$s0,	1		# Increment Outer pointer by 1
+	blt	$s0,	$t4,	sloop		# Check if the outer pointer < length of the data list. If true, perform a new loop. Next statement will be executed.
+	add 	$s1,	$s0,	$0		# Overwrite value of Inner pointer with the Outer pointer to avoid iterating over old values
 	
-	jr $ra
-	nop
+	jr 	$ra				# Return from jump
+	nop					# Avoid additional operation
+	
+	b
